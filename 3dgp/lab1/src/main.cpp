ca9974a793a3bb2e3322a36b5063391b5bbe51dd
Karsten Pedersen <kpedersen@z420.local>
2018-08-08 13:20:46 +0100

    Import of lab code.

---
+++ 3dgp/lab1/src/main.cpp
@@ -0,0 +1,38 @@
+#include <SDL2/SDL.h>
+
+#include <exception>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+int main(int argc, char *argv[])
+{
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    throw std::exception();
+  }
+
+  SDL_Window *window = SDL_CreateWindow("Triangle",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
+
+  bool quit = false;
+
+  while(!quit)
+  {
+    SDL_Event event = {0};
+
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
